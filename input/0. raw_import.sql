c977326fcabd2c45398e242bae2e692e29b2e391
AntoineGiraud <antoine.giraud@outlook.com>
2024-09-11 13:55:41 +0200

    feat: ajout variable env path_input grâce à 🦆 v1.1.0

--- input/0. raw_import.sql
+++ input/0. raw_import.sql
@@ -1,3 +1,10 @@
+select version();
+
+-- ctrl + enter dans dbeaver --> execute les lignes qui se touchent jq'au prochain ;
+set VARIABLE path_input = 'C:/Users/antoi\Documents\codes\dbt_hypermarche\input\';
+SELECT getvariable('path_input');
+
+
 
 CREATE SCHEMA IF NOT EXISTS raw;
 
@@ -5,11 +12,11 @@ CREATE SCHEMA IF NOT EXISTS raw;
 -- drop table retours;
 -- drop table personnes;
 create or replace table raw.raw_achats as
-    SELECT * FROM read_csv('input/achats.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true)
+    SELECT * FROM read_csv(getvariable('path_input') || 'achats.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true);
 create or replace table raw.raw_retours as
-    SELECT * FROM read_csv('input/retours.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true)
+    SELECT * FROM read_csv(getvariable('path_input') || 'retours.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true);
 create or replace table raw.raw_personnes as
-    SELECT * FROM read_csv('input/personnes.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true)
+    SELECT * FROM read_csv(getvariable('path_input') || 'personnes.csv', AUTO_DETECT=TRUE, header=true, all_varchar=true);
 
 SHOW all TABLES;
 -- describe raw.achats;
