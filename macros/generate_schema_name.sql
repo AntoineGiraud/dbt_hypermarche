db3e514907eb90f19b386d600894ae90c284b497
AntoineGiraud <antoine.giraud@outlook.com>
2025-11-24 17:01:35 +0100

    feat(profile): add dev/prod config & schema suffix if in dev

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
+        {{ target.schema | trim }}
     {%- else -%}
-
         {{ custom_schema_name | trim }}
-
     {%- endif -%}
+
+    {%- if target.name != "prod"  -%}
+    _{{ target.name }}
+    {%- endif -%}   
 
 {%- endmacro %}
